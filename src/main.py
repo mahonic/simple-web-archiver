d2d543f96f4b09f658808fcc7fe41ef5f946d7f6
Rosalia Mahonic <100803315+mahonic@users.noreply.github.com>
2024-08-25 15:37:01 +0200

    Add linter and format

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from config import get_settings
 
 _loger = logging.getLogger(__name__)
 _loger.addHandler(logging.StreamHandler())
-_loger.addHandler(logging.FileHandler(filename='log.txt'))
+_loger.addHandler(logging.FileHandler(filename="log.txt"))
 _loger.setLevel(logging.DEBUG)
 
 
@@ -20,7 +20,7 @@ class FailedToDownloadUrl:
     status_code: int
 
 
-def _download_url(url: str) -> tuple[str| None, FailedToDownloadUrl | None]:
+def _download_url(url: str) -> tuple[str | None, FailedToDownloadUrl | None]:
     response = requests.get(url)
     if response.status_code != 200:
         return None, FailedToDownloadUrl(url=url, status_code=response.status_code)
@@ -29,11 +29,14 @@ def _download_url(url: str) -> tuple[str| None, FailedToDownloadUrl | None]:
 
 def _extract_links_from_html(html: str, base_url: str) -> set[str]:
     # https://regex101.com/r/XyZ4eA/1
-    return {f"{base_url}{_match.group(1)}" for _match in re.finditer(r'<a ?href="(\/.+)">(.+)<\/a>', html)}
+    return {
+        f"{base_url}{_match.group(1)}"
+        for _match in re.finditer(r'<a ?href="(\/.+)">(.+)<\/a>', html)
+    }
 
 
 def _get_save_path(url: str) -> Path:
-    url = url.lstrip('https://').lstrip("http://").rstrip("/")
+    url = url.lstrip("https://").lstrip("http://").rstrip("/")
     paths = url.split("/")
     if len(paths) == 1:
         return Path(url) / "index.html"
@@ -44,10 +47,11 @@ def _save_page(url: str, html: str, output_dir: Path, base_url: str) -> Path:
     save_path = output_dir / _get_save_path(url)
     save_path.parent.mkdir(parents=True, exist_ok=True)
     # fix theme - lazy way to just point at the original url
-    html = re.sub(r'"(\/theme\/.+)"', rf'{base_url}\g<1>', html)
-    with save_path.open('w') as f:
+    html = re.sub(r'"(\/theme\/.+)"', rf"{base_url}\g<1>", html)
+    with save_path.open("w") as f:
         f.write(html)
     return save_path
+
 
 def main():
     settings = get_settings()
@@ -66,7 +70,9 @@ def main():
         visited_pages.add(url)
         if error:
             failed_downloads.append(error)
-            _loger.warning(f"Failed to save page {url}. Status code {error.status_code}")
+            _loger.warning(
+                f"Failed to save page {url}. Status code {error.status_code}"
+            )
             continue
 
         links_on_page = _extract_links_from_html(page_html, settings.base_url)
@@ -78,7 +84,11 @@ def main():
                     _loger.debug(f"Skipping url {link} due to {excluded_path}")
                     skipped_urls.add(link)
                     break
-            if link not in skipped_urls and link not in visited_pages and link not in pages_to_visit:
+            if (
+                link not in skipped_urls
+                and link not in visited_pages
+                and link not in pages_to_visit
+            ):
                 pages_to_visit.add(link)
 
         _save_page(url, page_html, settings.output_dir, settings.base_url)
@@ -87,8 +97,5 @@ def main():
     _loger.info(f"Downloaded {len(visited_pages)} pages! :3")
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
